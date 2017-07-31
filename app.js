b0f2e8abfc0410d28a5f0d54c04f4556f128778b
RB <rb@rbdev.uk>
2017-07-31 19:36:39 +0100

    trying to apply validation

---
+++ app.js
@@ -0,0 +1,24 @@
+// app.js
+
+// create angular app
+var validationApp = angular.module('validationApp', []);
+
+// create angular controller
+validationApp.controller('mainController', function($scope) {
+
+  // function to submit the form after all validation has occurred            
+  $scope.submitForm = function(isValid) {
+
+    // check to make sure the form is completely valid
+    if (isValid) {
+      
+      alert('Form is Valid');
+
+      // push form data to console here
+      console.log( $scope.userForm.data().serialize() ); 
+
+    }
+
+  };
+
+});
